b18b8fbc89e91a1aa14ce5a73eebd74946ea2992
comzac <tjstn921030@gmail.com>
2023-07-10 22:35:11 +0900

    [CHECK] reassessment of chilren componet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import Button from './components/UI/Button/Button'
 import './App.css';
+import Demo from './components/UI/Button/Demo/Demo';
 
 function App() {
+const [showParagraph, setShowParagraph] = useState(false);
+console.log('App Running!');
+const toggleParagraphHandler= () => {
+  setShowParagraph(prevShowParagraph => !prevShowParagraph);
+}
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <Demo show = {showParagraph}/>
+      <Button onClick={toggleParagraphHandler}> Toggle </Button>
     </div>
   );
 }
---
+++ src/components/UI/Button/Demo/Demo.js
@@ -0,0 +1,8 @@
+const Demo = props => {
+
+    console.log('Demo Running!')
+
+    return <p>{props.show ? 'this is new!':''}</p>
+};
+
+export default Demo;
